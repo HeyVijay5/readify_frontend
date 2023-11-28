5d175b83073ac759a915329fe1f391f9e7faaacb
HeyVijay5 <144220923+HeyVijay5@users.noreply.github.com>
2023-11-28 12:48:52 +0530

    AddTransaction.js

--- src/Pages/Dashboard/AdminDashboard/Components/AddTransaction.js
+++ src/Pages/Dashboard/AdminDashboard/Components/AddTransaction.js
@@ -9,7 +9,7 @@ import moment from "moment"
 
 function AddTransaction() {
 
-    const API_URL = "http://localhost:4000/";
+    const API_URL = "https://readify-backend.onrender.com/";
     const [isLoading, setIsLoading] = useState(false)
     const { user } = useContext(AuthContext)
 
